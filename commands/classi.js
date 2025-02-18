619a279560e77e0ef0eccf85430bde161aafd8c0
Lieno2 <98769977+Lieno2@users.noreply.github.com>
2025-02-18 13:23:26 +0100

    Update classi.js
    added the member in the server count and the max member in the class

--- commands/classi.js
+++ commands/classi.js
@@ -1,16 +1,38 @@
 const { SlashCommandBuilder } = require('discord.js')
 const client = require('../index.js').client
+const sqlite = require('better-sqlite3');
+const path = require('path');
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('classi').setDescription('Mostra la lista delle classi'),
 
 	async execute(interaction) {
+
+		const db = sqlite(`${__dirname}/../storage/db.sqlite`)
+		
+		function getStudentCountBySection(sectionName) {
+			const stmt = db.prepare(
+				`SELECT COUNT(students.id) AS student_count
+				 FROM sections
+				 LEFT JOIN students ON sections.id = students.section
+				 WHERE sections.name = ?`
+			);
+			const row = stmt.get(sectionName);
+			return row ? row.student_count : 0;
+		}
+		
+
 		const guild = await client.guilds.fetch(interaction.guild.id)
 		const classes = guild.roles.cache.filter((r) => !isNaN(Number(r.name[0]))).sort()
 		let list = ''
 
-		classes.forEach((c) => (list += `${c.name}\n`))
-
+		
+		classes.forEach((c) => {
+			
+			const maxSize = getStudentCountBySection(c.name);
+			list += `${c.name} [${c.members.size} / ${maxSize}]\n`;
+		})
+		
 		await interaction.reply({
 			embeds: [
 				{
@@ -21,5 +43,6 @@ module.exports = {
 			],
 			ephemeral: true
 		})
+		db.close();
 	}
 }
